37de2033c5b0ca0ff295dfa25d908048e7b38b82
Richard <richard.trow.davis@gmail.com>
2019-03-04 11:34:41 -0800

    Populated app.js with multiple occurences of HabitList.js - Added fetchHabits function to access API data from our Ruby server; data was successfully logged to console

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import Forms from './components/Forms.js'
 import './main.css'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      currentView: 'habits',
+      completedHabits: [],
+      habits: []
+    }
+  }
+
+  fetchHabits = () => {
+    fetch('http://localhost:3000/habits')
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData)
+    })
+    .catch(error => console.log(error))
+  }
+
+  componentDidMount () {
+    this.fetchHabits()
+  }
+
   render() {
     return (
       <React.Fragment>
         <div className="main-container">
           <h1>Hello Caroline!</h1>
-          <HabitList />
+          <Forms />
           <h3>Morning Habits</h3>
+            <HabitList />
           <h3>Afternoon Habits</h3>
+            <HabitList />
           <h3>Evening Habits</h3>
+            <HabitList />
         </div>
       </React.Fragment>
     );
