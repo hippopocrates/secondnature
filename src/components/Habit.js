7566acf5a91c8360930a3826e3ab9886071ae6df
Richard <richard.trow.davis@gmail.com>
2019-03-05 10:40:46 -0800

    Added stylings to each habit card, some styles are for test purposes such as shadow and buttons. Added animations so when one habit is clicked, it animates into the complete/incomplete sections

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -3,33 +3,37 @@ import React, { Component } from "react";
 class Habit extends Component {
   render() {
     return (
-      <div className="Habit">
-        <div className="habit-item">{this.props.habit.habit_item}</div>
-        <button
-          onClick={() => {
-            this.props.handleCheck(
-              this.props.habit,
-              this.props.arrayIndex,
-              this.props.currentArray
-            );
-          }}
-        >
-          {this.props.habit.completed === false ? "Complete" : "Incomplete"}
-        </button>
-
-        <button
-          onClick={() => {
-            this.props.handleDelete(
-              this.props.habit.id,
-              this.props.arrayIndex,
-              this.props.currentArray
-            );
-          }}
-        >
-          Delete
-        </button>
-      </div>
-    );
+      <React.Fragment>
+        <div className="Habit">
+          <div className="habit-item slide-in-blurred-left">{this.props.habit.habit_item}
+            <div className="habit-item-buttons">
+              <button
+                onClick={() => {
+                  this.props.handleCheck(
+                    this.props.habit,
+                    this.props.arrayIndex,
+                    this.props.currentArray
+                  );
+                }}
+              >
+                {this.props.habit.completed === false ? "Complete" : "Incomplete"}
+              </button>
+              <button
+                onClick={() => {
+                  this.props.handleDelete(
+                    this.props.habit.id,
+                    this.props.arrayIndex,
+                    this.props.currentArray
+                  );
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    )
   }
 }
 
